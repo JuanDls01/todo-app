535b4db6a7e0cc3ae96ba65e662af842e657afc1
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-10-16 18:23:39 -0300

    feat/footer

--- src/App.tsx
+++ src/App.tsx
@@ -18,50 +18,67 @@ function App() {
   };
 
   return (
-    <div className='h-screen w-96 flex flex-col m-4'>
-      <h1 className='text-6xl font-bold text-white mb-10'>Todo App</h1>
-      <TodoAdd onAdd={handleOnAddItem} />
-      <div>
+    <div className='w-screen flex flex-col justify-between items-center'>
+      <div className='h-screen w-96 flex flex-col m-4'>
+        <h1 className='text-6xl font-bold text-white mb-10'>Todo App</h1>
+        <TodoAdd onAdd={handleOnAddItem} />
         <div>
-          <h2 className='text-2xl font-bold text-white border-b-2 mb-2'>
-            To do
-          </h2>
           <div>
-            {todoList.map(
-              (todo, index) =>
-                !todo.done && (
-                  <TodoItem
-                    key={index}
-                    id={index}
-                    text={todo.text}
-                    done={todo.done}
-                    onChange={handleOnChangeDone}
-                  />
-                )
-            )}
+            <h2 className='text-2xl font-bold text-white border-b-2 mb-2'>
+              To do
+            </h2>
+            <div>
+              {todoList.map(
+                (todo, index) =>
+                  !todo.done && (
+                    <TodoItem
+                      key={index}
+                      id={index}
+                      text={todo.text}
+                      done={todo.done}
+                      onChange={handleOnChangeDone}
+                    />
+                  )
+              )}
+            </div>
           </div>
-        </div>
-        <div>
-          <h2 className='text-2xl font-bold text-white border-b-2 mb-2'>
-            Done
-          </h2>
           <div>
-            {todoList.map((todo, index) => {
-              return (
-                todo.done && (
-                  <TodoItem
-                    key={index}
-                    id={index}
-                    text={todo.text}
-                    done={todo.done}
-                    onChange={handleOnChangeDone}
-                  />
-                )
-              );
-            })}
+            <h2 className='text-2xl font-bold text-white border-b-2 mb-2'>
+              Done
+            </h2>
+            <div>
+              {todoList.map((todo, index) => {
+                return (
+                  todo.done && (
+                    <TodoItem
+                      key={index}
+                      id={index}
+                      text={todo.text}
+                      done={todo.done}
+                      onChange={handleOnChangeDone}
+                    />
+                  )
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
+
+      <footer className='flex w-screen text-white py-5 border-t items-center justify-center'>
+        <p>
+          Created by{" "}
+          <a
+            href='https://github.com/JuanDls01'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='font-semibold text-indigo-400 hover:underline'
+          >
+            Juan De los Santos
+          </a>{" "}
+          - Escuelita Crombie
+        </p>
+      </footer>
     </div>
   );
 }
